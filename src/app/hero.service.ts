863876b8252c3f43dad6f386e8957741cff83b3d
Adrian Li <li.adrianmc@gmail.com>
2017-01-11 10:42:51 -0500

    get individual hero with its own fetch

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -33,9 +33,14 @@ export class HeroService {
   }
 
   getHero(id: number): Promise<Hero> {
-    return this.getHeroes().then(
-      heroes => heroes.find(hero => hero.id === id)
-    )
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get(url)
+                    .toPromise()
+                    .then(res => res.json().data as Hero)
+                    .catch(this.handleError);
+    // return this.getHeroes().then(
+    //   heroes => heroes.find(hero => hero.id === id)
+    // )
   }
 
   constructor(private http: Http) { }
